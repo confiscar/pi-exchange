d8b7bd57742e865cbb73980b77efbfaf360c67ef
psyhs5 <psyhs5@nottingham.ac.uk>
2019-04-10 22:29:44 +0100

    Revert "get ip and port from file"
    This reverts commit 95e9d651998ae3a6e149941adc477710155d31d3.

--- src/BOT/src/bot/main.java
+++ src/BOT/src/bot/main.java
@@ -1,12 +1,8 @@
 package bot;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -18,43 +14,7 @@ public class main {
 	public static Object lock = new Object();
 	 public static void main(String[] args) throws UnknownHostException, IOException{  
 		 
-		 List<String> records = new ArrayList<String>();
-	        String filename = "config.txt";
-	        String IPAddress = null;
-	        int port = 0;
-		 
-		 try
-		 {
-		     BufferedReader reader = new BufferedReader(new FileReader(filename));
-		     String line;
-		     while ((line = reader.readLine()) != null)
-		     {
-		         records.add(line);
-		     }
-		     reader.close();
-
-		     for(int i = 0; i < records.size(); i++){
-		         if(records.get(i).contains("IP-Address")){
-		             String part[] = records.get(i).split(":");
-		             IPAddress = part[1].trim();
-		             System.out.println(IPAddress);
-		         }
-		         else if(records.get(i).contains("Port")){
-		             String part[] = records.get(i).split(":");
-		             port = Integer.parseInt(part[1].trim());
-		             System.out.println(port);
-		         }
-		     }
-		 }
-		 catch (Exception e)
-		 {
-		     System.err.format("Exception occurred trying to read '%s'.", filename);
-		     e.printStackTrace();
-		 }
-		 
-		 
-		 
-		Socket client = new Socket(IPAddress, port);
+		Socket client = new Socket("192.168.137.220", 8890);
 		
 		client.setSoTimeout(0);
 		
@@ -77,9 +37,3 @@ public class main {
         
 	 }
 }
-
-
-
-
-
-

fde93abf34126485b4e2b5a24da869e1b6e8384a
psyhs5 <psyhs5@nottingham.ac.uk>
2019-04-10 22:33:31 +0100

    get ip and port from file

--- src/BOT/src/bot/main.java
+++ src/BOT/src/bot/main.java
@@ -1,8 +1,12 @@
 package bot;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -14,8 +18,43 @@ public class main {
 	public static Object lock = new Object();
 	 public static void main(String[] args) throws UnknownHostException, IOException{  
 		 
+		 List<String> records = new ArrayList<String>();
+	        String filename = "config.txt";
+	        String IPAddress = null;
+	        int port = 0;
+		 
+		 try
+		 {
+		     BufferedReader reader = new BufferedReader(new FileReader(filename));
+		     String line;
+		     while ((line = reader.readLine()) != null)
+		     {
+		         records.add(line);
+		     }
+		     reader.close();
 
-		Socket client = new Socket("192.168.137.220", 8890);
+		     for(int i = 0; i < records.size(); i++){
+		         if(records.get(i).contains("IP-Address")){
+		             String part[] = records.get(i).split(":");
+		             IPAddress = part[1].trim();
+		             System.out.println(IPAddress);
+		         }
+		         else if(records.get(i).contains("Port")){
+		             String part[] = records.get(i).split(":");
+		             port = Integer.parseInt(part[1].trim());
+		             System.out.println(port);
+		         }
+		     }
+		 }
+		 catch (Exception e)
+		 {
+		     System.err.format("Exception occurred trying to read '%s'.", filename);
+		     e.printStackTrace();
+		 }
+		 
+		 
+		 
+		Socket client = new Socket(IPAddress, port);
 		
 		client.setSoTimeout(0);
 		
@@ -38,3 +77,9 @@ public class main {
         
 	 }
 }
+
+
+
+
+
+
